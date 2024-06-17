4ea96d2facb8cde1d7fde862e1b44f4297bb1f70
amir <amirporgoli138111@gmail.com>
2024-06-17 17:28:45 +0330

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,69 @@
+<template>
+    <v-footer color="#1f1f1f" class="d-flex flex-column">
+      <div class="px-4 py-2 bgtop w-100">
+        <v-toolbar color="transparent" height="40">
+          <span class="mr-2">Refund Policy</span>
+          <v-spacer></v-spacer>
+          <v-icon>mdi-cash-multiple</v-icon>
+          <v-icon>mdi-currency-usd</v-icon>
+          <v-icon>mdi-cash-100</v-icon>
+        </v-toolbar>
+      </div>
+      <div class="d-flex w-100 bgmidum px-4 pb-10">
+        <v-row>
+          <v-col cols="12" sm="3">
+            <h4 class="mt-2">Connect With us</h4>
+            <span class="text-caption">Facebook</span><br />
+            <span class="text-caption">Twitter</span><br />
+            <span class="text-caption">Instagram</span><br />
+            <span class="text-caption">Whatsapp</span><br />
+            <span class="text-caption">Youtube</span><br />
+            <span class="text-caption">Pinterest</span>
+          </v-col>
+          <v-col cols="12" sm="3">
+            <h4 class="mt-2">More About Store</h4>
+            <span class="text-caption">Multiple Branches</span><br />
+            <span class="text-caption">Take Franchise</span><br />
+            <span class="text-caption">Scheduled Offers</span><br />
+            <span class="text-caption">Visit Kitchen</span><br />
+            <span class="text-caption">Meet Our Team</span><br />
+            <span class="text-caption">More Links</span>
+          </v-col>
+          <v-col cols="12" sm="3">
+            <h4 class="mt-2">Offline Store</h4>
+            <span class="text-caption">Address: Mazandaran/Nour</span><br />
+            <span class="text-caption">Wheeling, West Virginia</span><br />
+            <span class="text-caption">26003</span><br />
+            <span class="text-caption">Contact: 09388125037</span><br />
+            <span class="text-caption">304-650-2694</span><br />
+            <span class="text-caption">Email: porgoliamir@gmail.com</span>
+          </v-col>
+          <v-col cols="12" sm="3">
+            <h4 class="mt-2">Location</h4>
+            <v-img src="/src/img/map.webp" height="150" class="ml-n16"></v-img>
+          </v-col>
+        </v-row>
+      </div>
+      <div class="px-4 py-2 bgtop text-center w-100">
+        <v-icon class="mr-2">mdi-copyright</v-icon>
+        {{ new Date().getFullYear() }} <strong>AmirHossin Porgoli</strong>
+      </div>
+    </v-footer>
+  </template>
+  
+  <script>
+  export default {};
+  </script>
+  
+  <style>
+  .v-footer {
+    padding: 0 !important;
+  }
+  .bgtop {
+    background-color: #111111;
+  }
+  .bgmidum {
+    background-color: #1f1f1f;
+  }
+  </style>
+  
---
+++ src/components/NavBar.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-system-bar color="#1f1f1f">
+    <span>Welcome to Foodie</span>
+    <v-spacer></v-spacer>
+    <v-icon class="ms-2" icon="mdi mdi-facebook"></v-icon>
+    <v-icon class="ms-2" icon="mdi mdi-linkedin"></v-icon>
+    <v-icon class="ms-2" icon="mdi mdi-pinterest"></v-icon>
+    <v-icon class="ms-2" icon="mdi mdi-twitter"></v-icon>
+    <v-icon class="ms-2" icon="mdi mdi-instagram"></v-icon>
+    <v-icon class="ms-2" icon="mdi mdi-youtube"></v-icon>
+  </v-system-bar>
+  <v-app-bar color="#1f1f1f" height="100" flat>
+    <v-app-bar-title>
+        <h2>Foodie</h2>
+    </v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-card
+      color="#141415"
+      flat
+      outlined
+      height="50"
+      width="500"
+      class="mt-6 pt-1"
+    >
+      <v-row>
+        <v-col cols="12" sm="10">
+          <v-text-field
+            density="compact"
+            placeholder="Search"
+            variant=""
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="2">
+          <v-icon
+            color="orange"
+            icon="mdi mdi-magnify"
+            size="large"
+            class="mt-2 ml-4"
+          ></v-icon>
+        </v-col>
+      </v-row>
+    </v-card>
+    <v-spacer></v-spacer>
+     <div>
+        <span class="text-caption ml-4">My Favorite</span><br/>
+            <v-btn append-icon="mdi mdi-heart-outline" class="account">
+                Wishlist
+                <template v-slot:append>
+                    <v-icon></v-icon>
+                </template>
+            </v-btn>
+     </div>
+     <v-divider vertical inset class="mr-6"></v-divider>
+     <div>
+        <span class="text-caption ml-4">Login/Signup</span><br />
+        <v-btn append-icon="mdi mdi-lock-open-outline" class="account">
+            My Account
+            <template v-slot:append>
+                <v-icon></v-icon>
+            </template>
+        </v-btn>
+     </div>
+     <v-divider vertical inset class="mr-6"></v-divider>
+     <v-icon class="mr-10" icon="mdi mdi-shopping-outline"></v-icon>
+  </v-app-bar>
+  <v-app-bar color="#191919" flat height="50">
+    <v-btn variant="text" color="orange">Home</v-btn>
+    <v-btn variant="text" class="orange">Shop</v-btn>
+    <v-btn variant="text" class="orange">Drinks</v-btn>
+    <v-btn variant="text" class="orange">Blog</v-btn>
+    <v-btn variant="text" class="orange">Contact Us</v-btn>
+    <v-btn variant="text" class="orange">Update to Pro</v-btn>
+    <v-spacer></v-spacer>
+    <v-btn icon="mdi mdi-phone" density="comfortable" class="phone mr-2"></v-btn>
+    <div>
+        <span class="text-caption">Call to</span><br />class
+        <span class="text-caption text-gray mr-2">09388125037</span>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.account {
+    transition-duration: 0.4s;
+}
+.account:hover {
+    color: orange;
+}
+.phone {
+    background-color: orange !important;
+}
+</style>
